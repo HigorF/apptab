405b3ad72dbab6fe43487d03bf43e34504ed6af4
HIGOR FOGACA SILVA <higor.fsilva3@senacsp.edu.br>
2019-09-18 20:08:12 -0300

    aplicativo mobile

---
+++ src/app/produtos/shared/produtos.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { FirebasePath } from 'src/app/core/shared/firebase-path';
+import { pipe } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProdutosService {
+
+  constructor(private db: AngularFireDatabase) { }
+
+  getAll(categoriaKey: string = null) {
+    return this.db.list(FirebasePath.PRODUTOS, q => {
+      if (categoriaKey) {
+        return q.orderByChild('categoriaKey').equalTo(categoriaKey);
+      } else {
+        return q.orderByChild('nome');
+      }
+    }).snapshotChanges().pipe(
+      map(changes => {
+        return changes.map(m => ({key: m.payload.key, ...m.payload.val() }));
+      })
+    )
+  }
+}
