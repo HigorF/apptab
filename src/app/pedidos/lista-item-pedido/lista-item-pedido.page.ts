ad2bc9edbdc42ea07bd45e9c5fdfc55da19b942d
HIGOR FOGACA SILVA <higor.fsilva3@senacsp.edu.br>
2019-09-27 22:12:37 -0300

    listagem e quantidade dos produtos do carrinho

--- src/app/pedidos/lista-item-pedido/lista-item-pedido.page.ts
+++ src/app/pedidos/lista-item-pedido/lista-item-pedido.page.ts
@@ -1,4 +1,7 @@
+import { AlertService } from './../../core/shared/alert.service';
+import { CarrinhoService } from './../shared/carrinho.service';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-lista-item-pedido',
@@ -6,10 +9,58 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lista-item-pedido.page.scss'],
 })
 export class ListaItemPedidoPage implements OnInit {
+itensPedido: Observable<any[]>;
+total: number;
 
-  constructor() { }
+  constructor(private carrinhoService: CarrinhoService, private alert: AlertService) { }
 
   ngOnInit() {
+    //trás os dados do carrinho, focando na key do produto
+    this.itensPedido = this.carrinhoService.getAll();
+    this.getTotalPedido();
   }
+
+  //exibir o valor de cada item no carrinho.
+  getTotalPedido(){
+    const subscribe = this.carrinhoService.getTotalPedido(). subscribe( (total: number) => {
+      subscribe.unsubscribe();
+      this.total = total;
+    })
+  }
+
+  //é a adição da quantidade do produto
+  adicionarQuantidade(itemPedido: any){
+    let qtd = itemPedido.quantidade;
+    qtd++;
+
+    this.atualizarTotal(itemPedido, qtd);
+  }
+
+  //remove quantidade de produtos/caso a quantidade chegar a zero irá fazer a remoção do produto do carrinho
+  removerQuantidade(itemPedido: any){
+    let qtd = itemPedido.quantidade;
+    qtd--;
+
+    if (qtd <=0){
+      this.removerProduto(itemPedido);
+    } else {
+      this.atualizarTotal(itemPedido, qtd);
+    }
+  }
+
+  //atualiza o preço total do pedido, a soma de todo o carrinho
+  atualizarTotal(itemPedido: any, quantidade: number){
+    const total = this.carrinhoService.calcularTotal(itemPedido.produtoPreco, quantidade);
+    this. carrinhoService.update(itemPedido.key, quantidade, total);
+    this.getTotalPedido();
+  }
+
+  //metodo que remove o item caso chegue a "0" a quantidade
+  removerProduto(itemPedido: any){
+    this.alert.ShowConfirmaExclusao(itemPedido.produtoNome, () =>{
+      this.carrinhoService.remove(itemPedido.key);
+      this.getTotalPedido();
+    })
+}
 
 }
--- src/app/pedidos/shared/carrinho.service.ts
+++ src/app/pedidos/shared/carrinho.service.ts
@@ -11,21 +11,64 @@ export class CarrinhoService {
 
   constructor(private db: AngularFireDatabase, private afAuth: AngularFireAuth) { }
 
+  //conexão da rota para adicionar o produto, concatenando a rota do carrinho/id do usuário/e o produto
   getCarrinhoProdutosRef() {
     const path = `${FirebasePath.CARRINHO}${this.afAuth.auth.currentUser.uid}/${FirebasePath.PRODUTOS}`;
     return this.db.list(path);
   }
 
+  //insere o produto no carrinho
   insert(itemProduto: any) {
     return this.getCarrinhoProdutosRef().push(itemProduto);
   }
 
+  //faz a consulta dentro do carrinho para saber se tem algum produto lá
   carrinhoPossuiItens(){
     return this.getCarrinhoProdutosRef().snapshotChanges().pipe(
     map(changes => {
       return changes.length > 0
     })
     )
+  }
+
+  //retorna o valor da operação
+  calcularTotal(preco: number, quantidade: number){
+    return preco * quantidade;
+  }
+
+  //encontra o produto pela key e recebe dados para serem alterados
+  update(key: string, quantidade: number, total: number){
+    return this.getCarrinhoProdutosRef(). update(key, {quantidade: quantidade, total: total})
+  }
+
+  //serve para remover o item selecionado, que o banco trás pela key
+  remove(key: string){
+    return this.getCarrinhoProdutosRef(). remove(key);
+  }
+
+  
+  getAll(){
+    return this.getCarrinhoProdutosRef(). snapshotChanges(). pipe(
+      map(changes => {
+        return changes.map(m => ({key: m.payload.key, ...m.payload.val() }) )
+      })
+    )
+  }
+
+  getTotalPedido(){
+    return this.getCarrinhoProdutosRef(). snapshotChanges(). pipe(
+      map(changes => {
+        return changes
+        .map( (m: any) => (m.payload.val(). total))
+        .reduce( (prev: number, current: number) => {
+          return prev + current;
+        })
+      })
+    )
+  }
+
+  clear(){
+
   }
 
 }
